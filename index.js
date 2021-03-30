fe49170e8d8f4deefea12987c5489204250917cb
Sophie Shin <sophie@alloy.co>
2021-03-30 19:09:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+function getFirstSelector(selector) {
+let element = document.querySelector(selector)
+return element
+}
 
+function nestedTarget() {
+let x = document.querySelector('#nested .target')
+return x
+}
+
+function increaseRankBy(n) {
+let y = document.querySelectorAll('ul.ranked-list li')
+}
