18afe26c858a5623652df6a3d66a5d08464ac45c
Sophie Shin <sophie@alloy.co>
2021-03-30 21:39:40 +0000

    Done.

--- index.js
+++ index.js
@@ -10,8 +10,17 @@ return x
 
 function increaseRankBy(n) {
 let y = document.querySelectorAll('ul.ranked-list li')
+for (let i=0; i<y.length; i++){
+  //parseInt(y[i].innerHTML) = y + n
+  y[i].innerHTML= parseInt(y[i].innerHTML) + n
+}
 }
 
 function deepestChild() {
-
-}
+  let a = document.querySelector('#grand-node')
+  let i = 0
+  while (i < a.length) {
+    a[i].innerHTML
+    i++
+    }
+  }
